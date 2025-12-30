10ac8cf9ba2849dc161988a1dbeb39680d630658
Sadegh Nassiri <123846488+sadegh-n@users.noreply.github.com>
2025-12-29 16:07:10 -0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,9 +14,9 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
-	cst_create_date DATE
+	cst_create_date DATETIME
 );
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
@@ -26,8 +26,8 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 );
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
@@ -46,7 +46,7 @@ IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
 	cid NVARCHAR(50),
-	bdate DATE,
+	bdate DATETIME,
 	gen NVARCHAR(50)
 );
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
