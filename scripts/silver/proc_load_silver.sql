d55fe826d4d3ce7f067f397bce7d53ac6998bc6e
Sadegh Nassiri <123846488+sadegh-n@users.noreply.github.com>
2025-12-29 16:10:50 -0800

    Create proc_load_silver.sql
    TODO: Finish 4/6 tables

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,74 @@
+/*
+================================================================================
+Stored Procedure: Load Silver Layer
+================================================================================
+Script Purpose:
+  This procedure loads data from bronze schema into silver schema.
+  It truncates the tables before cleaning data with queries.
+Parameters:
+  This procedure has no parameters and has no return value. 
+Example:
+  EXEC silver.load_silver
+================================================================================
+*/
+TRUNCATE TABLE silver.crm_cust_info
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date)
+
+SELECT
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		 ELSE 'n/a'
+	END cst_marital_status,
+	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		 ELSE 'n/a'
+	END cst_gndr,
+	cst_create_date
+FROM (
+SELECT 
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as rn
+FROM
+	bronze.crm_cust_info
+WHERE
+	cst_id IS NOT NULL
+)a WHERE rn = 1
+
+TRUNCATE TABLE silver.crm_prd_info
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key,7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line)) 
+		 WHEN 'M' THEN 'Mountain'
+	     WHEN 'R' THEN 'Road'
+		 WHEN 'S' THEN 'Other Sales'
+		 WHEN 'T' THEN 'Touring'
+		 ELSE 'n/a'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM
+	bronze.crm_prd_info
